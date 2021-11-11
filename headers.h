23cebbf0a6b0585e8ad05adaf6ed585c44b4b121
William L. Thomson Jr <wlt@o-sinc.com>
2021-11-10 23:45:14 -0500

    headers.h: Initial headers file with some includes and main prototypes
    - Intial includes for common libraries and ones used in main
    - Prototypes in main for nmalloc and printError along with documentation

---
+++ headers.h
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+/*** main prototypes ***/
+
+/**
+ * Allocates memory and null fills the memory
+ *
+ * @param size the amount of memory to be allocated
+ *
+ * @return a void pointer to the first address of allocated memory
+ */
+void* nmalloc(size_t size);
+
+/**
+ * Print an error
+ *
+ * @param filename the filename of the file being parsed
+ * @param line the line number of the file the error occurred on
+ * @param format a formatted string
+ * @param ... variable arguments to match the format (va_list)
+ */
+void printError(const char *filename, const int line, const char *format, ...);
+
