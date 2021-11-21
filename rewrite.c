fca5163650c7f2d7f7b02e1f2f0fe814e71e4e2b
houmank <43026293+houmank@users.noreply.github.com>
2021-11-20 23:59:36 -0500

    rewrite.c: changed the arguments passed into rewritesSICTRecord

--- rewrite.c
+++ rewrite.c
@@ -1,6 +1,13 @@
 #include "headers.h"
 
-int rewriteSICTRecord(TRECORD* record, int* start, int mAddress)
+int rewriteSICTRecord(TRECORD* record, int oldStart, int newStart, int mAddress)
 {
+	// TOOD: find the correct t-record
 
+	// process the t-record
+	char* left, right;
+	left = record->data + 1;
+	right = left + 6;
+	int currentAddress = strtol(left, &right, 16);
+	sprintf(left, "%0*X", 6, (currentAddress - oldStart) + newStart);
 }
