f54ff00942eaed41dc7036b39f21b1bfc0bf485f
houmank <43026293+houmank@users.noreply.github.com>
2021-11-22 18:55:37 -0500

    rewrite.c: added the code to relocate a t-record given an m-record address.
    - the current code could be changed for further improvement. Right now this code assumes the m-record wants 4 half bytes changed for the normal SIC object code.
    - The parsing of the t-record address and t-record size metadata is being done every function call, and multiple times depending on the size of the list and desired t-record. This can be fixed by doing these calculations when building the TRECORD objects and the list so that it is only done once for each record.

--- rewrite.c
+++ rewrite.c
@@ -1,13 +1,51 @@
 #include "headers.h"
 
+#define RECORD_ADDR_OFFSET 1
+#define RECORD_SIZE_OFFSET 7
+#define RECORD_OBJ_OFFSET 9
+#define NUM_HALF_BYTES_TO_EDIT 4
+#define X_FLAG_BIT (1 << 15)
+
 int rewriteSICTRecord(TRECORD* record, int oldStart, int newStart, int mAddress)
 {
-	// TOOD: find the correct t-record
+	// find the correct t-record
+	int currentRecordAddress;
+	char* left;
+
+	while (record != NULL)
+	{
+		left = record->data + RECORD_ADDR_OFFSET;
+		sscanf(left, "%06X", &currentRecordAddress);
+
+		left = record->data + RECORD_SIZE_OFFSET;
+		int recordSize;
+		sscanf(left, "%02X", &recordSize);
+
+		if (mAddress >= currentRecordAddress && mAddress <= (currentRecordAddress + recordSize))
+			break;
+
+		record = record->next;
+	}
+
+	if (record == NULL)
+		return 1;
 
 	// process the t-record
-	char* left, right;
-	left = record->data + 1;
-	right = left + 6;
-	int currentAddress = strtol(left, &right, 16);
-	sprintf(left, "%0*X", 6, (currentAddress - oldStart) + newStart);
+	int offset = mAddress - currentRecordAddress;
+
+	left = record->data + RECORD_OBJ_OFFSET + (offset * 2);
+	int objAddress;
+	sscanf(left, "%0*X", NUM_HALF_BYTES_TO_EDIT, &objAddress); // assuming the number of half bytes we are editing is 4
+
+	char xBitSet = objAddress & X_FLAG_BIT;
+	objAddress = objAddress & (X_FLAG_BIT - 1);
+
+	unsigned int relocatedAddress = (objAddress - oldStart) + newStart;
+	relocatedAddress |= xBitSet;
+
+	char tempBuffer[NUM_HALF_BYTES_TO_EDIT + 1];
+	sprintf(tempBuffer, "%0*X", NUM_HALF_BYTES_TO_EDIT, relocatedAddress); // do this so sprintf doesnt copy over the \0
+	strncpy(left, tempBuffer, NUM_HALF_BYTES_TO_EDIT);
+
+	return 0;
 }
