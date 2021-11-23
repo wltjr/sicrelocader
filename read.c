f046f4426f2f0ff04df3fb0bb7d3173c59b714cd
William L. Thomson Jr <wlt@o-sinc.com>
2021-11-23 18:27:53 -0500

    read.c: Added H and E records to the linked list along with T records
    - Modified to add the header and footer records to the linked list along
      with the text records
    - Header and footer records are modified with new start address and new
      first executable address
    - Intial support for reading in modification records, to be hooked into
      rewrite for rewriting the text records

--- read.c
+++ read.c
@@ -16,9 +16,12 @@ TRECORD* readFile(char* filename, int* start_new)
     TRECORD* first = NULL;
     TRECORD* last = NULL;
     int TREC_LEN = 71;
-    int HEADER_LEN = 20;
     int line_num = 0;
-    char header[20] = {0};
+    int maddress = 0;
+    int mod_len = 0;
+    int prog_len = 0;
+    int start_old = 0;
+    int exec_old = 0;
     char line[71] = {0};
 
     // directory check
@@ -41,19 +44,16 @@ TRECORD* readFile(char* filename, int* start_new)
             printError(filename, -1, "File not found or could not be read");
         return NULL;
     }
-    
-    // get header record
-    if(fgets(line, TREC_LEN, fp)) {
-        line_num++;
-        strncpy(header,line, HEADER_LEN);
-    }
 
     // get t-records, build linked list
     while (fgets(line, TREC_LEN, fp))
     {
         line_num++;
+        // handle text record 84 = T
         if(line[0]==84) {
-            TRECORD* trec = nmalloc(sizeof(TRECORD));
+            TRECORD* trec;
+
+            trec = nmalloc(sizeof(TRECORD));
             trec->size = strlen(line)+1;
             trec->data = nmalloc(trec->size * sizeof(char));
             strncpy(trec->data, line, trec->size);
@@ -63,6 +63,69 @@ TRECORD* readFile(char* filename, int* start_new)
             else
                 last->next = trec;
             last = trec;
+        }
+        // handle modification record 77 = M
+        else if(line[0]==77) {
+            char *skip;
+
+            skip = line + RECORD_ADDR_OFFSET;
+            sscanf(skip,"%06X",&maddress);
+            skip = line + RECORD_SIZE_OFFSET;
+            sscanf(skip,"%02X",&mod_len);
+#ifdef DEBUG
+            printf("mod-address = %X , len = %X \n", maddress, mod_len);
+#endif
+        }
+        // handle header record 72 = H
+        else if(line[0]==72) {
+            TRECORD* trec;
+            char *skip;
+
+            skip = line + RECORD_SIZE_OFFSET;
+            sscanf(skip,"%06X",&start_old);         // store old start address
+            skip = line + RECORD_SIZE_OFFSET * 2;
+            sscanf(skip,"%06X",&prog_len);          // store program length
+
+            trec = nmalloc(sizeof(TRECORD));
+            trec->size = strlen(line)+1;
+            trec->data = nmalloc(trec->size * sizeof(char));
+            // copy first part of header, H + NAME
+            strncpy(trec->data, line, RECORD_SIZE_OFFSET);
+            // add the new start + same length to header
+            skip = trec->data + RECORD_SIZE_OFFSET;
+            sprintf(skip,"%06X%06X\n",*start_new,prog_len);
+            trec->line = line_num;
+            if(!first)
+                first = trec;
+            else
+                last->next = trec;
+            last = trec;
+#ifdef DEBUG
+            printf("start old = %X , prog len = %X \n", start_old, prog_len);
+#endif
+        }
+        // handle end record 69 = E
+        else if(line[0]==69) {
+            TRECORD* trec;
+            char *skip;
+
+            skip = line + RECORD_ADDR_OFFSET;
+            sscanf(skip,"%06X",&exec_old);
+
+            trec = nmalloc(sizeof(TRECORD));
+            trec->size = strlen(line)+1;
+            trec->data = nmalloc(trec->size * sizeof(char));
+            // calculate new address of first line to execute
+            sprintf(trec->data,"H%06X\n",(*start_new + (exec_old - start_old)));
+            trec->line = line_num;
+            if(!first)
+                first = trec;
+            else
+                last->next = trec;
+            last = trec;
+#ifdef DEBUG
+            printf("exec old = %X\n", exec_old);
+#endif
         }
     }
 
