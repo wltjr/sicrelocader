6292ab5e22a196889fe6732c609fda38f3015d51
William L. Thomson Jr <wlt@o-sinc.com>
2021-11-23 19:25:32 -0500

    read.c: Added addRecord function to centralize adding records to the list
    - Function eliminates duplicated code and puts it in a reusable place, with
      the main changes to the data of the record taking place outside after this
      function is called creating the new record and adding to the list. Along
      with updating first and last list pointers.

--- read.c
+++ read.c
@@ -2,6 +2,33 @@
 
 #define DEBUG
 
+
+/**
+ * Adds a record to the linked list, with memory allocated based on line size
+ *
+ * @line the line of the file of the record to add to the linked list
+ * @line_num pointer to the variable tracking the line number
+ * @first reference to the pointer tracking the first record in the linked list
+ * @last reference to the pointer tracking the last record in the linked list
+ *
+ * @return a pointer to the new TRECORD struct in the linked list
+ */
+TRECORD* addRecord(char *line, int *line_num, TRECORD** first, TRECORD** last)
+{
+    TRECORD* trec;
+
+    trec = nmalloc(sizeof(TRECORD));
+    trec->size = strlen(line)+1;
+    trec->data = nmalloc(trec->size * sizeof(char));
+    trec->line = *line_num;
+    if(!*first)
+        *first = trec;
+    else
+        (*last)->next = trec;
+    *last = trec;
+    return(trec);
+}
+
 /**
  * Reads a sic object file and loads the text records into a linked list
  *
@@ -53,16 +80,9 @@ TRECORD* readFile(char* filename, int* start_new)
         if(line[0]==84) {
             TRECORD* trec;
 
-            trec = nmalloc(sizeof(TRECORD));
-            trec->size = strlen(line)+1;
-            trec->data = nmalloc(trec->size * sizeof(char));
+            trec = addRecord(line, &line_num, &first, &last);
             strncpy(trec->data, line, trec->size);
-            trec->line = line_num;
-            if(!first)
-                first = trec;
-            else
-                last->next = trec;
-            last = trec;
+            printf("%s", trec->data);
         }
         // handle modification record 77 = M
         else if(line[0]==77) {
@@ -86,20 +106,12 @@ TRECORD* readFile(char* filename, int* start_new)
             skip = line + RECORD_SIZE_OFFSET * 2;
             sscanf(skip,"%06X",&prog_len);          // store program length
 
-            trec = nmalloc(sizeof(TRECORD));
-            trec->size = strlen(line)+1;
-            trec->data = nmalloc(trec->size * sizeof(char));
+            trec = addRecord(line, &line_num, &first, &last);
             // copy first part of header, H + NAME
             strncpy(trec->data, line, RECORD_SIZE_OFFSET);
             // add the new start + same length to header
             skip = trec->data + RECORD_SIZE_OFFSET;
             sprintf(skip,"%06X%06X\n",*start_new,prog_len);
-            trec->line = line_num;
-            if(!first)
-                first = trec;
-            else
-                last->next = trec;
-            last = trec;
 #ifdef DEBUG
             printf("start old = %X , prog len = %X \n", start_old, prog_len);
 #endif
@@ -112,17 +124,9 @@ TRECORD* readFile(char* filename, int* start_new)
             skip = line + RECORD_ADDR_OFFSET;
             sscanf(skip,"%06X",&exec_old);
 
-            trec = nmalloc(sizeof(TRECORD));
-            trec->size = strlen(line)+1;
-            trec->data = nmalloc(trec->size * sizeof(char));
+            trec = addRecord(line, &line_num, &first, &last);
             // calculate new address of first line to execute
             sprintf(trec->data,"H%06X\n",(*start_new + (exec_old - start_old)));
-            trec->line = line_num;
-            if(!first)
-                first = trec;
-            else
-                last->next = trec;
-            last = trec;
 #ifdef DEBUG
             printf("exec old = %X\n", exec_old);
 #endif
