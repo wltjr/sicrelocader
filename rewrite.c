f413e2bfdd778acddfb2aca45f254b3b8782b9a6
houmank <43026293+houmank@users.noreply.github.com>
2021-11-23 21:02:47 -0500

    rewrite.c: fixed a bug where the xbit was not being read and segfaults where happening on gcc

--- rewrite.c
+++ rewrite.c
@@ -6,44 +6,47 @@
 
 int rewriteSICTRecord(TRECORD* record, int oldStart, int newStart, int mAddress)
 {
-	// find the correct t-record
-	int currentRecordAddress;
-	char* left;
+    // find the correct t-record
+    int currentRecordAddress = 0;
+    char* left;
 
-	while (record != NULL)
-	{
-		left = record->data + RECORD_ADDR_OFFSET;
-		sscanf(left, "%06X", &currentRecordAddress);
+    while (record != NULL)
+    {
+        left = record->data + RECORD_ADDR_OFFSET;
+        if (sscanf(left, "%06X", &currentRecordAddress) == 0)
+            return 1;
 
-		left = record->data + RECORD_SIZE_OFFSET;
-		int recordSize;
-		sscanf(left, "%02X", &recordSize);
+        left = record->data + RECORD_SIZE_OFFSET;
+        int recordSize = 0;
+        if (sscanf(left, "%02X", &recordSize) == 0)
+            return 1;
 
-		if (mAddress >= currentRecordAddress && mAddress <= (currentRecordAddress + recordSize))
-			break;
+        if (mAddress >= currentRecordAddress && mAddress <= (currentRecordAddress + recordSize))
+            break;
 
-		record = record->next;
-	}
+        record = record->next;
+    }
 
-	if (record == NULL)
-		return 1;
+    if (record == NULL)
+        return 1;
 
-	// process the t-record
-	int offset = mAddress - currentRecordAddress;
+    // process the t-record
+    int offset = mAddress - currentRecordAddress;
 
-	left = record->data + RECORD_OBJ_OFFSET + (offset * 2);
-	int objAddress;
-	sscanf(left, "%0*X", NUM_HALF_BYTES_TO_EDIT, &objAddress); // assuming the number of half bytes we are editing is 4
+    left = record->data + RECORD_OBJ_OFFSET + (offset * 2);
+    unsigned int objAddress = 0;
+    if (sscanf(left, "%04X", &objAddress) == 0) // assuming the number of half bytes we are editing is 4
+        return 1;
 
-	char xBitSet = objAddress & X_FLAG_BIT;
-	objAddress = objAddress & (X_FLAG_BIT - 1);
+    unsigned int xBit = objAddress & X_FLAG_BIT;
+    objAddress = objAddress & (X_FLAG_BIT - 1);
 
-	unsigned int relocatedAddress = (objAddress - oldStart) + newStart;
-	relocatedAddress |= xBitSet;
+    unsigned int relocatedAddress = (objAddress - oldStart) + newStart;
+    relocatedAddress |= xBit;
 
-	char tempBuffer[NUM_HALF_BYTES_TO_EDIT + 1];
-	sprintf(tempBuffer, "%0*X", NUM_HALF_BYTES_TO_EDIT, relocatedAddress); // do this so sprintf doesnt copy over the \0
-	strncpy(left, tempBuffer, NUM_HALF_BYTES_TO_EDIT);
+    char tempBuffer[NUM_HALF_BYTES_TO_EDIT + 1] = { 0 };
+    sprintf(tempBuffer, "%04X", relocatedAddress); // do this so sprintf doesnt copy over the \0
+    strncpy(left, tempBuffer, NUM_HALF_BYTES_TO_EDIT);
 
-	return 0;
+    return 0;
 }
