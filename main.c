d60a53a0b60ca8192885c0414910b8852ce20674
Riyam Yatooma <n01479311@cisvm-softeng1.ccec.unf.edu>
2021-11-27 20:07:35 -0500

    main.c : argp- added option validation

--- main.c
+++ main.c
@@ -23,12 +23,15 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     switch(key) {
 	 case 'd': printf (".\n");
         break;
-	case 'f': printf("f\n");
-	 break;
-	case 's': printf("s\n");
-	 break;
-	case 'p': printf("p\n");
-	 break;
+	case 'f':
+		{filename = state->argv[1];
+        	 break;}
+	case 's':
+	       	{start = state->argv[2];
+        	 break;}
+	case 'p':
+	        {platform = state->argv[3];
+        	 break;}
         case ARGP_KEY_NO_ARGS:
             if(!state->argv[1])
                 argp_usage(state);
