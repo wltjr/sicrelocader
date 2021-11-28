120f0fe64cd573a53350d24f8696ab2253adfb79
Riyam Yatooma <riyamyatooma@gmail.com>
2021-11-28 13:36:32 -0500

    main.c: Added commented out call to writeFile

--- main.c
+++ main.c
@@ -74,7 +74,10 @@ int main(int argc, char* argv[])
     int start_new;
     sscanf(argv[2], "%06X", &start_new);
     RECORD* first = readFile(argv[1], &start_new);
-
+	
+/* FIXME: uncomment
+    writeFile(filename,first);
+/
     RECORD* cur = first;
     while(cur) {
         RECORD* link = cur;
